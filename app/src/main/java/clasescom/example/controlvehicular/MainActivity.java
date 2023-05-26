6cd60cbcab383d0d73563f587f446c72c2df7ce8
Jonathan Marquez <jmp120801@micorreo.upp.edu.mx>
2023-05-26 06:56:11 -0600

    primer avance- semana19

---
+++ app/src/main/java/clasescom/example/controlvehicular/MainActivity.java
@@ -0,0 +1,14 @@
+package clasescom.example.controlvehicular;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
